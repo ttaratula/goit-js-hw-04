1e1dbd9b126d373bdbfb471ad6f79f01ef719bf5
Tetiana Taratula <ttaratula@gmail.com>
2025-01-27 18:49:19 +0000

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+function slugify(title) {
+   
+    const LowerCaseTitle = title.toLowerCase();
+  
+    const slug = LowerCaseTitle.split(" ").join('-');
+  
+    return slug;
+  }
+
+
+
+console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
---
+++ js/task-3.js
@@ -0,0 +1,22 @@
+function filterArray(numbers, value){
+
+  const emptyArray =[];
+
+  for (let number of numbers) {
+    if (number > value) {
+      emptyArray.push(number);
+    }
+  }
+
+  return emptyArray;
+
+
+}
+
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
